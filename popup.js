a2e0e11e481e95903b74f93d62c71843674b9818
Guy Novaes <guynovaes@petrobras.com.br>
2020-05-31 10:31:36 -0300

    Possibilidade de selecionar a qtd de numeros por bilhete

--- popup.js
+++ popup.js
@@ -1,14 +1,21 @@
-// Script feito por Guy Novaes
+// Script por por Guy Novaes
 // Extensão para o google Chrome para incluir jogos no site de Loterias Online da Caixa (https://www.loteriasonline.caixa.gov.br/).
 // É comum a Caixa modificar o site dela e ser necessário adequar este script. Este script não possui garantia alguma de funcionamento.
 // Não comunique ao autor que o script deixou de funcionar. Sinta-se à vontade para alterá-lo e distribuí-lo gratuitamente.
 // Script sem garantia. Use por sua conta e risco.
 
 // função que clica nos números na DOM
-function modifyDOM(linha) {
+
+
+function modifyDOM(linha, quantidade) {
 
 	var volante = linha.split(' ');
 	document.getElementById('limparvolante').click();
+
+	// clica no botão aumentarnumero a quantidade de vezes que o usuário selecionou
+	for(var i = 0;i < quantidade; i++){
+		document.getElementById('aumentarnumero').click();
+	}
 
 	for(var i = 0;i < volante.length;i++){
 		var v;
@@ -34,9 +41,9 @@ function modifyDOM(linha) {
 }
 
 // função para mandar executar no DOM a função marca os números.
-function marcaJogo(linha) {
+function marcaJogo(linha, quantidade) {
 	chrome.tabs.executeScript({
-		code: "(" + modifyDOM + ")('"+linha+"');"
+		code: "(" + modifyDOM + ")('"+ linha +"','" + quantidade + "' );"
 	}, (results) => {
 		console.log('erro');
 	});
@@ -49,8 +56,11 @@ var button = document.getElementById('mybutton');
 // atribui a ação onclick do clicar no botão 
 button.onclick = function() {
 	var lines = document.getElementById("listadejogos").value.split('\n');
+	var e = document.getElementById("quantidade");
+	var quantidadeAMarcar = e.options[e.selectedIndex].value;
+
 	for(var i = 0;i < lines.length;i++){
-		marcaJogo( lines[i] );
+		marcaJogo( lines[i], quantidadeAMarcar  );
 	};
 }
 
